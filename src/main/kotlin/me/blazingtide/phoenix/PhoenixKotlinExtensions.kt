3c1f5ebf0d56a0aff279be34e1d242f8920496e0
BlazingTide <blazingtide@gmail.com>
2022-08-26 14:37:41 -0700

    Better implementation of buttons in Kotlin

--- src/main/kotlin/me/blazingtide/phoenix/PhoenixKotlinExtensions.kt
+++ src/main/kotlin/me/blazingtide/phoenix/PhoenixKotlinExtensions.kt
@@ -1,4 +1,13 @@
 package me.blazingtide.phoenix
 
-class PhoenixKotlinExtensions {
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.inventory.ItemStack
+
+inline fun Menu.button(item: ItemStack, slot: Int, crossinline listener: InventoryClickEvent.() -> Unit) {
+    populator()
+        .clicked {
+            listener.invoke(it)
+        }.item(item)
+        .slot(slot)
+        .create()
 }
