67c62848eca322dd88533c55f5a0701cebc85cd2
BlazingTide <blazingtide@gmail.com>
2022-08-27 16:05:54 -0700

    Some ideas for better kotlin creation of menus

--- src/main/kotlin/me/blazingtide/phoenix/PhoenixKotlinExtensions.kt
+++ src/main/kotlin/me/blazingtide/phoenix/PhoenixKotlinExtensions.kt
@@ -1,13 +1,39 @@
 package me.blazingtide.phoenix
 
+import me.blazingtide.phoenix.button.IButton
+import me.blazingtide.phoenix.config.ConfigMenu
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.inventory.ItemStack
 
-inline fun Menu.button(item: ItemStack, slot: Int, crossinline listener: InventoryClickEvent.() -> Unit) {
-    populator()
+inline fun Menu.button(item: ItemStack, slot: Int, crossinline listener: InventoryClickEvent.() -> Unit): IButton {
+    return populator()
         .clicked {
             listener.invoke(it)
         }.item(item)
         .slot(slot)
         .create()
 }
+
+inline fun ConfigMenu.defineAction(id: String, crossinline handler: InventoryClickEvent.() -> Unit) {
+    defineAction(id) {
+        handler.invoke(it)
+    }
+}
+
+
+/*
+
+val menu = Menu {
+    title("Something")
+    size(MenuSize.ROWS_6)
+
+    drawButtons {
+        button(item, slot) {
+            //Do stuff
+        }
+    }
+}
+
+val configMenu = ConfigMenu("location").open()
+
+ */
