d97c524214d7c0d53c99758abfbdaa7347756b96
BlazingTide <45052821+BlazingTide@users.noreply.github.com>
2022-08-27 22:33:38 -0700

    Fix issue with plugin

--- src/main/java/me/blazingtide/phoenix/PhoenixPlugin.java
+++ src/main/java/me/blazingtide/phoenix/PhoenixPlugin.java
@@ -6,6 +6,11 @@ public class PhoenixPlugin extends JavaPlugin {
 
     private static Phoenix phoenix;
 
+    @Override
+    public void onEnable() {
+        phoenix = new Phoenix(this);
+    }
+
     public static Phoenix getPhoenix() {
         return phoenix;
     }
