6dfacf6ea4ba2ce9357d22aa4e4d1e2ab575804a
BlazingTide <45052821+BlazingTide@users.noreply.github.com>
2022-09-17 15:28:53 -0700

    Resolve issues w/ reloading config

--- src/main/java/me/blazingtide/phoenix/config/MenuConfig.java
+++ src/main/java/me/blazingtide/phoenix/config/MenuConfig.java
@@ -1,31 +1,28 @@
 package me.blazingtide.phoenix.config;
 
 import lombok.Getter;
-import me.blazingtide.phoenix.Phoenix;
 import me.blazingtide.phoenix.utils.PhoenixColorTranslator;
 import org.bukkit.Material;
-import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 
 import java.io.File;
-import java.io.IOException;
 
 public class MenuConfig {
 
     private final File file;
     @Getter
-    private final FileConfiguration config;
+    private FileConfiguration config;
 
     public MenuConfig(String filePath) {
         file = new File(filePath);
         config = YamlConfiguration.loadConfiguration(file);
     }
 
-    public void reload() throws IOException, InvalidConfigurationException {
-        config.load(file);
+    public void reload() {
+        config = YamlConfiguration.loadConfiguration(file);
     }
 
     public ItemStack constructItem(String path) {
